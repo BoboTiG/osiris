2b9ac0575d6ecf497a7d67c7bd18b38cb30a1975
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2018-09-25 10:08:35 +0200

    Remove --debug and use long format options in osiris.sh

--- osiris.sh
+++ osiris.sh
@@ -3,5 +3,5 @@
 export LD_LIBRARY_PATH="$HOME/.local/lib/:$HOME/.local/lib64/"
 source ../envars.env
 source ../venv370/bin/activate
-python3 -m osiris -c rules.ini -d || true
+python3 -m osiris --config-file rules.ini || true
 deactivate
