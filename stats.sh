8c7fddb249462851a582710fc5756251c86731c7
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2018-10-09 11:13:11 +0200

    Enhance the sats.sh script
    - Sort output by date DESC
    - Add a optional ARG to limit the number of outputed lines

--- stats.sh
+++ stats.sh
@@ -1,10 +1,23 @@
 #!bin/bash
 
-# Print number of actions by day.
-sql="\
-   SELECT strftime('%Y-%m-%d', run_at) d, action, SUM(count) \
-     FROM osiris \
- GROUP BY d, action \
-"
 
-sqlite3 statistics.db "${sql};"
+show() {
+    # Print actions counts day by day DESC.
+    # Oprional ARG is the number of lines to output.
+
+    local ndays sql
+
+    [ -z "$1" ] && lines="10" || lines="$1"
+
+    sql="\
+  SELECT strftime('%Y-%m-%d', run_at) d, action, SUM(count) \
+    FROM osiris \
+GROUP BY d, action \
+ORDER BY d DESC \
+   LIMIT ${lines} \
+;"
+
+    sqlite3 statistics.db "${sql}"
+}
+
+show $@
