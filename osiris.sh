aba46cd2a73f05c1250522ee0314630f0cd9a92a
Mickaël Schoentgen <contact@tiger-222.fr>
2019-09-05 15:16:44 +0200

    Fix processes not killed when called from cron

--- osiris.sh
+++ osiris.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH="$HOME/.local/lib/:$HOME/.local/lib64/"
 source ../envars.env
-source ../venv370/bin/activate
-python3 -m osiris --config-file rules.ini || true
-deactivate
+LD_LIBRARY_PATH="$HOME/.local/lib/:$HOME/.local/lib64/" ../venv370/bin/python3 -m osiris --config-file rules.ini
